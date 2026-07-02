7fa06513a0db1682be4aea23f6989c8ebd125c17
KaleAkshay1 <153808715+KaleAkshay1@users.noreply.github.com>
2026-07-02 10:48:09 +0000

    start lerning singly linked list

---
+++ Linked_List/Singly_linked_list/index.js
@@ -0,0 +1,144 @@
+class Node {
+  constructor (data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+
+  getVal(){
+    return this.data;
+  }
+}
+
+class LinkedList {
+  constructor () {
+    this.head = null;
+  }
+
+  insertAtStart (val) {
+    const newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  insertAtLast (val) {
+    const newNode = new Node(val);
+    let num = this.head;
+    if(num === null){
+      this.head = newNode;
+    }else{
+      while(num.next !== null){
+        num = num.next;
+      }
+      num.next = newNode;
+    }
+    
+  }
+
+  insertAtMidAfterPosition(val, position) {
+    let num = this.head;
+    let newNode = new Node(val);
+
+    if (num === null) {
+        return;
+    }
+
+    while (num !== null) {
+        if (num.data === position) {
+            newNode.next = num.next;
+            num.next = newNode;
+            return;
+        }
+
+        num = num.next;
+    }
+  }
+
+  insertAtMidBeforePosition(val, position) {
+    let num = this.head;
+
+    if (num === null) {
+        return;
+    }
+
+    while (num !== null) {
+        if (num.data === position) {
+            num.next = new Node(num.data, num.next);
+            num.data = val;
+            return;
+        }
+
+        num = num.next;
+    }
+  }
+
+  deleteAtStart() {
+    if(this.head === null) return;
+    this.head = this.head.next;
+  };
+
+  deleteAtEnd() {
+    if(this.head === null) return;
+    let num = this.head;
+    if(num.next === null){
+      this.head = null;
+    }
+    while(num.next.next !== null) {
+      num = num.next;
+    }
+    num.next = null
+  };
+
+  deleteSpecificNode(node) {
+    let num = this.head;
+    if(num === null) return;
+    if(num.data === node){
+      this.head = num.next;
+      return;
+    }
+    let prev = null;
+    while(num !== null){
+      if(num.data === node){
+        prev.next = num.next;
+        return
+      }
+
+      prev = num;
+      num = num.next;
+    }
+  }
+
+  display() {
+    let num = this.head;
+
+    if (num === null) {
+        return "Empty List";
+    }
+
+    let str = "";
+
+    while (num !== null) {
+        str += num.data;
+
+        if (num.next !== null) {
+            str += " -> ";
+        }
+
+        num = num.next;
+    }
+
+    return str;
+  }
+}
+
+const LL = new LinkedList();
+LL.insertAtStart(1)
+LL.insertAtStart(2)
+LL.insertAtStart(3)
+LL.insertAtLast(5)
+LL.insertAtLast(4)
+LL.insertAtMidAfterPosition(11,1)
+LL.insertAtMidBeforePosition(12,1)
+LL.deleteAtStart()
+LL.deleteAtEnd()
+LL.deleteSpecificNode(12)
+console.log(LL.display());
